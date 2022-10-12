7edc6ecc2c4a6e59f2441beafda73168637f33dc
maybewp <wahyu.perwira@accenture.com>
2022-10-12 15:58:04 +0700

    Add Extend

--- src/main/java/com/wahyu/perwira/learnrestapiv1/LearnRestapiv1Application.java
+++ src/main/java/com/wahyu/perwira/learnrestapiv1/LearnRestapiv1Application.java
@@ -2,12 +2,19 @@ package com.wahyu.perwira.learnrestapiv1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearnRestapiv1Application {
+public class LearnRestapiv1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnRestapiv1Application.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.source(LearnRestapiv1Application.class);
+		return builder.sources(LearnRestapiv1Application.class);
+	}
 }
